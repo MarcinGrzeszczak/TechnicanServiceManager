93015d3e5e9831f1cba08d36e99569f188f7b938
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-10-29 19:15:24 +0100

    Added linter

--- src/main/java/mg/technicanservicemanager/App.java
+++ src/main/java/mg/technicanservicemanager/App.java
@@ -6,8 +6,10 @@ package mg.technicanservicemanager;
  */
 public class App 
 {
+    String a;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("This is a very long string that should be reduced a bit because it is over 120 characters.");
     }
 }
