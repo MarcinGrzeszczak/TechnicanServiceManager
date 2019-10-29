7344c156484f439b6c13d860186acb158ff9e856
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-10-29 17:52:28 +0100

    Initial commit

---
+++ src/main/java/mg/technicanservicemanager/App.java
@@ -0,0 +1,13 @@
+package mg.technicanservicemanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
