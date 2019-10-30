72cbe9edd075ba814805b725d0ad25c985576f9a
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-10-30 01:56:55 +0100

    Added quick methods

--- src/main/java/mg/backend/Entity.java
+++ src/main/java/mg/backend/Entity.java
@@ -1,9 +1,30 @@
 package mg.backend;
 
-abstract class Entity {
-    String name;
-    long id;
+abstract class Entity implements DatabaseContract {
+    protected String name;
+    protected long id;
     
+    Entity(long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     abstract String show();
 
 
