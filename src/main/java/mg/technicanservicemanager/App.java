b3096ad2c1fba111a9c3062966231b5ff8ccc934
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-10-30 01:02:57 +0100

    Fixed linter issues

--- src/main/java/mg/technicanservicemanager/App.java
+++ src/main/java/mg/technicanservicemanager/App.java
@@ -4,12 +4,8 @@ package mg.technicanservicemanager;
  * Hello world!
  *
  */
-public class App 
-{
-    String a;
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("This is a very long string that should be reduced a bit because it is over 120 characters.");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
