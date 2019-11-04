350521a4f1fdd34cfee22df8dafc16ea19e0eae8
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-11-04 12:28:54 +0100

    created factory instances

--- src/main/java/mg/backend/BackendFasade.java
+++ src/main/java/mg/backend/BackendFasade.java
@@ -7,20 +7,44 @@ import java.util.stream.Collectors;
 
 import mg.backend.database.DatabaseConnection;
 import mg.backend.entities.ClientEntity;
+import mg.backend.entities.CostEntity;
+import mg.backend.entities.DeviceEntity;
+import mg.backend.entities.HistoryEntity;
 import mg.backend.factories.ClientFactory;
+import mg.backend.factories.CostFactory;
+import mg.backend.factories.DeviceFactory;
 import mg.backend.factories.EntityListFactory;
+import mg.backend.factories.HistoryFactory;
 
 public class BackendFasade {
     final String config = "resources/config.properties";
 
     private DatabaseConnection dbConn;
 
-    EntityListFactory<ClientFactory, ClientEntity> clientFactory;
-    List<ClientEntity> clients;
+    private EntityListFactory<ClientFactory, ClientEntity> clientsFactory;
+    private EntityListFactory<DeviceFactory, DeviceEntity> devicesFactory;
+    private EntityListFactory<HistoryFactory, HistoryEntity> historyFactory;
+    private EntityListFactory<CostFactory, CostEntity> costsFactory;
+    
+    private List<ClientEntity> clients;
+    private List<DeviceEntity> devices;
+    private List<HistoryEntity> history;
+    private List<CostEntity> costs;
 
     public BackendFasade() {
-        this.clientFactory = new EntityListFactory<>(new ClientFactory(), dbConn);
+
+        this.clientsFactory = 
+            new EntityListFactory<>(new ClientFactory(), dbConn);
         
+        this.devicesFactory = 
+            new EntityListFactory<DeviceFactory,DeviceEntity>(new DeviceFactory(), dbConn);
+
+        this.historyFactory =
+            new EntityListFactory<HistoryFactory,HistoryEntity>(new HistoryFactory(), dbConn);
+        
+        this.costsFactory =
+            new EntityListFactory<CostFactory,CostEntity>(new CostFactory(), dbConn);
+
     }
 
     public void initConnection() throws IOException, SQLException {
@@ -34,6 +58,6 @@ public class BackendFasade {
     }
 
     public void loadClients() throws SQLException {
-        this.clients = (List<ClientEntity>) this.clientFactory.createList();
+        this.clients = this.clientsFactory.createList();
     }
 }
