ca5a62a3d5afa5d010b59854ea029435105940f7
Marcin Grzeszczak <marcingrzeszczak@Air-Marcin.lan>
2019-11-21 13:41:05 +0100

    Cleaning code

--- src/main/java/mg/technicanservicemanager/App.java
+++ src/main/java/mg/technicanservicemanager/App.java
@@ -3,9 +3,12 @@ package mg.technicanservicemanager;
 import mg.frontend.console.Console;
 
 public class App {
-    public static void main(String[] args) {
-        new Console();
-        //consoleFrontend.mainLoop();
 
+    public App() {
+        new Console();
+    }
+    
+    public static void main(String[] args) { 
+        App app = new App();
     }
 }
